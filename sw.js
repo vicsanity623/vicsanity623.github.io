63d0812653b786f562173d08fc1e795006c57f0d
vicsanity623 <125237471+vicsanity623@users.noreply.github.com>
2025-06-28 23:03:08 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // A version number for your cache. Change this when you update files.
-const CACHE_VERSION = 'v4.5.5'; // Use a new version number
+const CACHE_VERSION = 'v4.7.7'; // Use a new version number
 const CACHE_NAME = `tap-guardian-cache-${CACHE_VERSION}`;
 
 // A list of the essential files your game needs to work.
