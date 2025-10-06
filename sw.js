869ff7b001d73d61ff081eb5c4247de18677621e
vicsanity623 <125237471+vicsanity623@users.noreply.github.com>
2025-10-06 06:58:35 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
-// A version number for your cache. Change this when you update files.
-const CACHE_VERSION = 'v1.0.4'; // Use a new version number
+// A version number for your cache. Change this to a new value every time you update any of the files.
+const CACHE_VERSION = 'v1.0.5.0'; // <-- Increment this number for every update!
 const CACHE_NAME = `tap-guardian-cache-${CACHE_VERSION}`;
 
-// A list of the essential files your game needs to work.
-// ONLY list files that actually exist in your project right now.
+// A list of the essential files your game needs to run offline.
 const FILES_TO_CACHE = [
-  '/', // Caches the root of your site
+  './', // Caches the root of your site
   './rpg.html',
   './game.js',
-  // Add 'rift.js' and other files here ONLY when they are created and uploaded.
+  // Add other essential files here if you have them
   './player.PNG',
   './clouds.PNG',
   './egg.png',
@@ -27,21 +26,21 @@ self.addEventListener('install', (event) => {
         return cache.addAll(FILES_TO_CACHE);
       })
       .then(() => {
+        // --- THIS IS THE FIX ---
         // This forces the waiting service worker to become the active service worker.
-        console.log('[ServiceWorker] Skip waiting on install');
+        console.log('[ServiceWorker] Skip waiting and activate immediately.');
         return self.skipWaiting();
       })
   );
 });
 
-// The 'activate' event is fired when the service worker is activated.
-// This is a good place to clean up old caches.
+// The 'activate' event is fired when the service worker becomes active.
 self.addEventListener('activate', (event) => {
   console.log('[ServiceWorker] Activate');
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        // If a cache's name is not our current cache name, delete it.
+        // Delete all old caches that don't match the current version.
         if (key !== CACHE_NAME) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
@@ -49,26 +48,20 @@ self.addEventListener('activate', (event) => {
       }));
     })
     .then(() => {
-        // This tells the service worker to take control of the page immediately.
-        console.log('[ServiceWorker] Claiming clients');
-        return self.clients.claim();
+      // --- THIS IS THE FIX ---
+      // This tells the new service worker to take control of all open pages.
+      console.log('[ServiceWorker] Claiming clients.');
+      return self.clients.claim();
     })
   );
 });
 
-// The 'fetch' event is fired for every network request the page makes.
+// The 'fetch' event is fired every time the browser tries to request a resource.
 self.addEventListener('fetch', (event) => {
-  // We only want to handle GET requests.
-  if (event.request.method !== 'GET') {
-    return;
-  }
-  
   event.respondWith(
-    // Cache-first strategy: Check the cache for a response to this request.
     caches.match(event.request)
       .then((response) => {
-        // If a response is found in the cache, return it.
-        // Otherwise, fetch the resource from the network.
+        // If the file is in the cache, serve it. Otherwise, fetch it from the network.
         return response || fetch(event.request);
       })
   );
